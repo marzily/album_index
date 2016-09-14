0853929264ee23fb6d0e21252ee0948b3bf61b41
margie <ly.margarett@gmail.com>
2016-09-13 18:22:21 -0700

    regenerate album model to change "name" to title

--- db/migrate/20160914012017_create_albums.rb
+++ db/migrate/20160914012017_create_albums.rb
@@ -1,7 +1,7 @@
 class CreateAlbums < ActiveRecord::Migration
   def change
     create_table :albums do |t|
-      t.string :name
+      t.string :title
       t.references :artist, index: true, foreign_key: true
 
       t.timestamps null: false
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160914011639) do
+ActiveRecord::Schema.define(version: 20160914012017) do
 
   create_table "albums", force: :cascade do |t|
-    t.string   "name"
+    t.string   "title"
     t.integer  "artist_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
