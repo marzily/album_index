bf396887042319fa27a422ff691012f1a32b5649
margie <ly.margarett@gmail.com>
2016-09-13 20:10:06 -0700

    create validations for song

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,5 +1,6 @@
 class Artist < ActiveRecord::Base
   has_many :albums, dependent: :destroy
+  has_many :songs, dependent: :destroy
   
   validates :name, presence: true
   validates_uniqueness_of :name, case_sensitive: false
--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -1,5 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe Song, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:artist) {
+    Artist.create(name: 'Lenny')
+  }
+
+  let(:album) {
+    artist.albums.create(title: 'Jets')
+  }
+
+  let(:song) {
+    album.songs.new(title: 'Lenny and the Jets', artist_id: artist.id)
+  }
+
+  it 'is valid with valid attributes' do
+    expect(song).to be_valid
+  end
+
+  it 'is not valid if title is missing' do
+    song.title = nil
+
+    expect(song).to_not be_valid
+  end
+
+  it 'is not valid if title is an empty string' do
+    song.title = ''
+
+    expect(song).to_not be_valid
+  end
+
+  it 'must belong to an artist' do
+    song.artist_id = nil
+
+    expect(song).to_not be_valid
+  end
+
+  it 'must belong to an artist in the database' do
+    song.artist_id = 400
+
+    expect(song).to_not be_valid
+  end
+
 end
