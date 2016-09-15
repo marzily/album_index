54eb44bc174cabfdc961cc5092e49c7bb2688b82
margie <ly.margarett@gmail.com>
2016-09-15 11:49:38 -0700

    update seed file to downcase all titles and names

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,13 @@
 10.times do
-  Artist.create(name: Faker::Name.name)
+  Artist.create(name: Faker::Name.name.downcase)
 end
 
 100.times do
-  Album.create(title: Faker::Hipster.word.capitalize, artist_id: rand(10) + 1)
+  Album.create(title: Faker::Hipster.word, artist_id: rand(10) + 1)
 end
 
 500.times do
   artist_id = rand(10) + 1
   album_id = Artist.find(artist_id).albums.sample.id
-  Song.create(title: Faker::Book.title, album_id: album_id, artist_id: artist_id)
+  Song.create(title: Faker::Book.title.downcase, album_id: album_id, artist_id: artist_id)
 end
